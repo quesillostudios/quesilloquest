f42506e65d10ca05b316a5b2856ee0c9bd226ce8
David Pino <aesthezel@gmail.com>
2022-06-16 01:55:52 -0400

    Add rotation and camera follow

--- Assets/QuesilloStudios/Entity/Movement.cs
+++ Assets/QuesilloStudios/Entity/Movement.cs
@@ -7,13 +7,21 @@ namespace QuesilloStudios.Entity
     public class Movement : MonoBehaviour
     {
         private CharacterController _characterController;
+
+        private float _axisX;
+        private float _axisZ;
+        private bool _jumpPressed;
+        
         private Vector3 _direction;
+        private Vector3 _gravityDirection;
+        private float _rotationAngle;
 
         [SerializeField] private Animator bodyAnimator;
-        [SerializeField] private float speed;
+        [SerializeField] private float movementSpeed;
+        [SerializeField] private float rotationSpeed;
         [SerializeField] private float jumpPower;
         [SerializeField] private float gravity;
-        
+
         private static readonly int IsWalking = Animator.StringToHash("IsWalking");
 
         private bool _jumpPerformed;
@@ -22,51 +30,71 @@ namespace QuesilloStudios.Entity
         {
             _characterController = GetComponent<CharacterController>();
         }
-        
-        void Update()
+
+        private void FixedUpdate()
         {
             Gravity();
             Jump();
+            Rotate();
             Displacement();
+        }
+        
+        void Update()
+        {
+            InputDetection();
+        }
+        
+        private void LateUpdate()
+        {
+            if (!_characterController.isGrounded) return;
+            bodyAnimator.SetBool(IsWalking, _axisZ != 0f);
+        }
+
+        private void InputDetection()
+        {
+            _axisX = Input.GetAxis("Horizontal");
+            _axisZ = Input.GetAxis("Vertical");
+            _jumpPressed = Input.GetKey(KeyCode.Space);
         }
 
         private void Displacement()
         {
-            _direction.x = Input.GetAxis("Horizontal") * speed;
-            _direction.z = Input.GetAxis("Vertical") * speed;
-
+            //_direction.x = Input.GetAxis("Horizontal") * speed;
+            _direction = transform.forward * (_axisZ * movementSpeed);
+            _direction += _gravityDirection;
+            
             _characterController.Move(_direction * Time.deltaTime);
+        }
+
+        private void Rotate()
+        {
+            _rotationAngle = _axisX * rotationSpeed;
+            transform.Rotate(Vector3.up * (_rotationAngle * (Time.deltaTime)));
         }
 
         private void Jump()
         {
-            if (!Input.GetKey(KeyCode.Space) || !_characterController.isGrounded) return;
-            //_direction.y += jumpPower;
-            StartCoroutine(PerformingJump());
+            if (!_characterController.isGrounded) return;
+            
+            if(_jumpPressed)
+                StartCoroutine(PerformingJump());
         }
 
         private void Gravity()
         {
             if (_jumpPerformed) return;
-            
-            _direction.y = _characterController.isGrounded ? 0f : -gravity;
-        }
-
-        private void LateUpdate()
-        {
-            if (!_characterController.isGrounded) return;
-            bodyAnimator.SetBool(IsWalking, (_direction.x != 0f || _direction.z != 0f));
+            _gravityDirection.y = _characterController.isGrounded ? 0f : -gravity;
         }
 
         private IEnumerator PerformingJump()
         {
             _jumpPerformed = true;
             
-            var distanceOfJump = 1f;
+            var distanceOfJump = 0f;
 
             while (distanceOfJump < jumpPower)
             {
-                _direction.y = distanceOfJump;
+                _gravityDirection.y = distanceOfJump;
                 distanceOfJump++;
                 yield return new WaitForEndOfFrame();
             }
