ca59132c2e5e463a8c087c3938764101839d87c7
David Pino <aesthezel@gmail.com>
2022-06-22 11:11:58 -0400

    Added PlayerMovement

--- Assets/QuesilloStudios/Entity/PlayerMovement.cs
+++ Assets/QuesilloStudios/Entity/PlayerMovement.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // ! VARIABLES PUBLICAS SE PUEDEN VER EN EL INSPECTOR DE UNITY
     public Animator animator;
     public float speed;
+    public float runningSpeed;
     public float rotationSpeed;
+    public float gravityForce;
+    // ! <--->
 
     private Vector3 _direction;
+    private Vector3 _gravityDirection;
     private float _rotationAngle;
 
     private float _axisX;
     private float _axisZ;
     private bool _jumpPressed;
+    private bool _runningPressed;
 
     private CharacterController _characterController;
 
@@ -30,13 +36,15 @@ public class PlayerMovement : MonoBehaviour
     {
         ReadInputs();
 
+        Gravity();
         Rotate();
         Movement();
     }
 
     private void LateUpdate()
     {
-        animator.SetBool("IsWalking", _direction.z != 0);
+        animator.SetBool("IsRunning", _direction.z != 0 && _runningPressed);
+        animator.SetBool("IsWalking", _direction.z != 0 && !_runningPressed);
     }
     // ! FIN DE FLUJO DE UNITY
 
@@ -44,12 +52,14 @@ public class PlayerMovement : MonoBehaviour
     {
         _axisX = Input.GetAxis("Horizontal");           // A - D o <- - ->
         _axisZ = Input.GetAxis("Vertical");             // W - S o abajo y arriba
-        _jumpPressed = Input.GetKey(KeyCode.Space);     // TRUE cuando presionemos espacio
+        _jumpPressed = Input.GetKeyDown(KeyCode.Space);     // TRUE cuando presionemos espacio
+        _runningPressed = Input.GetKey(KeyCode.LeftShift);
     }
 
     private void Movement()
     {
-        _direction = transform.forward * (_axisZ * speed);
+        _direction = transform.forward * (_axisZ * (_runningPressed == true ? runningSpeed : speed));
+        _direction += _gravityDirection; // Ejerciendo la gravedad al movimiento
         _characterController.Move(_direction * Time.deltaTime);
     }
 
@@ -57,5 +67,11 @@ public class PlayerMovement : MonoBehaviour
     {
         _rotationAngle = _axisX * rotationSpeed;
         transform.Rotate(Vector3.up * (_rotationAngle * Time.deltaTime));
+    }
+
+    private void Gravity()
+    {
+        _gravityDirection.y = _characterController.isGrounded == true ? 0f : -gravityForce;
+        // ! Azucar sintactico para una pregunta de SI o NO
     }
 }
